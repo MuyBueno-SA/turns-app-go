4570057ed51fbee4ba3bff647159849099cb7bf3
Federico Bogado <ficobogado@gmail.com>
2024-05-09 17:36:33 -0300

    Go back on reflect (Algy's fault)

--- utils/time_test.go
+++ utils/time_test.go
@@ -1,13 +1,12 @@
 package utils
 
 import (
-	"reflect"
 	"testing"
 	"time"
 )
 
-func assertDateTime(t *testing.T, expected TimeRange, actual TimeRange) {
-	if reflect.DeepEqual(expected, actual) == false {
+func assertTimeRange(t *testing.T, expected TimeRange, actual TimeRange) {
+	if expected != actual {
 		t.Errorf("Expected %v, got %v", expected, actual)
 	}
 }
@@ -23,21 +22,21 @@ func TestGetWeekByDay(t *testing.T) {
 		day := time.Date(2024, 2, 29, 0, 0, 0, 0, time.UTC)
 		week := GetWeekByDay(day)
 
-		assertDateTime(t, weekRange, week)
+		assertTimeRange(t, weekRange, week)
 	})
 
 	t.Run("test week monday", func(t *testing.T) {
 		day := time.Date(2024, 2, 26, 0, 0, 0, 0, time.UTC)
 		week := GetWeekByDay(day)
 
-		assertDateTime(t, weekRange, week)
+		assertTimeRange(t, weekRange, week)
 	})
 
 	t.Run("test week sunday", func(t *testing.T) {
 		day := time.Date(2024, 3, 3, 23, 59, 59, 0, time.UTC)
 		week := GetWeekByDay(day)
 
-		assertDateTime(t, weekRange, week)
+		assertTimeRange(t, weekRange, week)
 	})
 
 }
